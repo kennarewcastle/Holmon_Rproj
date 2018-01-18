45965c77e6a33535767c48912a41e6be8cb269b1
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-01-18 16:44:27 -0500

    Created bacterial growth graphs

--- holmon_R.R
+++ holmon_R.R
@@ -56,4 +56,34 @@ litterBact<-bact_dat$litter_treatment
 bact_growth<-bact_dat$bacterial_growth
 dayBact<-bact_dat$day
 
-# Create figure, bacterial growth over incubation
+# Create figure, bacterial growth over incubation (no points, clean)
+ggplot(data=bact_dat,aes(x=dayBact,y=bact_growth,color=litterBact,linetype=grazingBact)) +
+  geom_smooth(fill=NA) +
+  scale_colour_hue(labels=c("Aspen","Empetrum","Lichen","Moss"),name="Litter Added") +
+  scale_linetype_discrete(labels=c("Grazed","Moss-Dominated","Ungrazed"),name="Grazing Cond.") +
+  labs(x= "Incubation Day", y= "Bacterial Growth Rate (picomoles leucine/g soil/hr) ") +
+  theme(legend.position="top",
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        axis.text=element_text(colour="black",size=10),
+        axis.title=element_text(size=14,face="bold"),
+        panel.border=element_rect(fill=NA,colour="black",size=1.5),
+        panel.background=element_rect(fill=NA)
+        )
+
+# Create figure, bacterial growth over incubation (points, messy)
+ggplot(data=bact_dat,aes(x=dayBact,y=bact_growth,color=litterBact,linetype=grazingBact,shape=grazingBact)) +
+  geom_smooth(fill=NA) +
+  geom_point() +
+  scale_colour_hue(labels=c("Aspen","Empetrum","Lichen","Moss"),name="Litter Added") +
+  scale_linetype_discrete(labels=c("Grazed","Moss-Dominated","Ungrazed"),name="Grazing Cond.") +
+  scale_shape_discrete(labels=c("Grazed","Moss-Dominated","Ungrazed"),name="Grazing Cond.") +
+  labs(x= "Incubation Day", y= "Bacterial Growth Rate (picomoles leucine/g soil/hr) ") +
+  theme(legend.position="top",
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        axis.text=element_text(colour="black",size=10),
+        axis.title=element_text(size=14,face="bold"),
+        panel.border=element_rect(fill=NA,colour="black",size=1.5),
+        panel.background=element_rect(fill=NA)
+        )
