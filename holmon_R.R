e0e75d457796b575929ea13b081a8bb7fbccd313
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-01-18 14:25:36 -0500

    Started bacterial growth graphs

--- holmon_R.R
+++ holmon_R.R
@@ -18,20 +18,6 @@ library(ggplot2)
 ggplot(data=resp_dat,aes(x=day,y=resp,color=litter,linetype=grazing,shape=grazing)) +
   geom_smooth(fill=NA) +
   geom_point() +
-  theme(legend.position="top",
-        #scale_linetype_discrete()
-        panel.grid.minor=element_blank(),
-        panel.grid.major=element_blank(),
-        axis.text=element_text(colour="black",size=10),
-        axis.title=element_text(size=14,face="bold"),
-        panel.border=element_rect(fill=NA,colour="black",size=1.5),
-        panel.background=element_rect(fill=NA)
-        )
-
-# Graph with points (messy)
-ggplot(data=resp_dat,aes(x=day,y=resp,color=litter,linetype=grazing,shape=grazing)) +
-  geom_smooth(fill=NA) +
-  geom_point() +
   scale_colour_hue(labels=c("Aspen","Empetrum","Lichen","Moss"),name="Litter Added") +
   scale_linetype_discrete(labels=c("Grazed","Moss-Dominated","Ungrazed"),name="Grazing Cond.") +
   scale_shape_discrete(labels=c("Grazed","Moss-Dominated","Ungrazed"),name="Grazing Cond.") +
@@ -60,3 +46,14 @@ ggplot(data=resp_dat,aes(x=day,y=resp,color=litter,linetype=grazing)) +
         panel.background=element_rect(fill=NA)
         )
 
+##### Bacterial growth (averaged over 4 replicates) over time
+
+# Create bacterial growth data frame
+library(dplyr)
+bact_dat<-filter(raw_dat,bacterial_growth!="NA")
+grazingBact<-bact_dat$grazing_treatment
+litterBact<-bact_dat$litter_treatment
+bact_growth<-bact_dat$bacterial_growth
+dayBact<-bact_dat$day
+
+# Create figure, bacterial growth over incubation
