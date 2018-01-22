e9ff2a3a11952c538ba7982c4f19c432d3935511
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-01-22 13:52:20 -0500

    Working on cumulative respiration, bacterial growth, fungal growth function (not working yet)

--- holmon_R.R
+++ holmon_R.R
@@ -129,3 +129,70 @@ ggplot(data=fung_dat,aes(x=dayFung,y=fung_growth,color=litterFung,linetype=grazi
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
         panel.background=element_rect(fill=NA)
         )
+
+##### Create function to calculate cumulative resp, bacterial growth, and fungal growth over incubation
+
+cumulative_response<-function(dat,response_name){
+  library(dplyr)
+  
+  rep6<-filter(dat,replicate_numb==6)
+  rep1<-filter(dat,replicate_numb==1)
+  rep8<-filter(dat,replicate_numb==8)
+  rep7<-filter(dat,replicate_numb==7)
+  
+  response6<-rep6$response_name
+  response1<-rep1$response_name
+  response8<-rep8$response_name
+  response7<-rep7$response_name
+  
+  day6<-rep6$day
+  day1<-rep1$day
+  day8<-rep8$day
+  day7<-rep7$day
+  
+  N6<-length(response6)
+  N1<-length(response1)
+  N8<-length(response8)
+  N7<-length(response7)
+  
+  response_rep6<-c()
+  response_rep8<-c()
+  response_rep7<-c()
+  response_rep1<-c()
+  
+  for (i in 1:N6) {
+    inc_growth6<-response6[i]*(day6[i]-day6[(i-1)])
+    response_rep6[i]<-inc_growth6
+  }
+  
+  for (j in 1:N1){
+    inc_growth1<-response1[j]*(day1[j]-day1[(j-1)])
+    response_rep1[j]<-inc_growth1
+  }
+  
+  for (k in 1:N8) {
+    inc_growth8<-response8[k]*(day8[k]-day8[(k-1)])
+    response_rep8[k]<-inc_growth8
+  }
+  
+  for (l in 1:N7) {
+    inc_growth7<-response7[l]*(day7[l]-day7[(l-1)])
+    response_rep7[l]<-inc_growth7
+  }
+  
+  
+  return(rep6_total=sum(response_rep6), rep1_total=sum(response_rep1), 
+                    rep8_total=sum(response_rep8), rep7_total=sum(response_rep7))
+
+}
+
+##### Test function for resp, bact growth, and fungal growth
+
+# Respiration
+resp_dat<-filter(raw_dat,resp_rate_ug_CO2.g_soil.h.!="NA") # "dat" data frame that function will work with
+real_respdat<-filter(resp_dat,day>0)
+
+cumulative_response(dat=resp_dat, response_name=resp_rate_ug_CO2.g_soil.h.)
+
+bact_df<-data.frame(rep6=c(), rep1=c(), rep8=c(), rep7=c())
+fung_df<-data.frame(rep6=c(), rep1=c(), rep8=c(), rep7=c())
